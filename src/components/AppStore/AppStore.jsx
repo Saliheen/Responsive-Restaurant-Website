5d08cb54b8a4789387a9138d15a80ba7513555fa
haseeb <haseeb.tech007@gmail.com>
2024-01-07 23:08:58 +0500

    initial commit

---
+++ src/components/AppStore/AppStore.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AppStoreImg from "../../assets/app_store.png";
+import PlayStoreImg from "../../assets/play_store.png";
+import Gif from "../../assets/mobile_bike.gif";
+
+const AppStore = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default AppStore;
---
+++ src/components/Navbar/DarkMode.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import darkPng from "../../assets/website/dark-mode-button.png";
+import lightPng from "../../assets/website/light-mode-button.png";
+
+const DarkMode = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+  const element = document.documentElement;
+
+  useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+  return (
+    <>
+      <div className="relative ">
+        <img src={darkPng} className="w-12" />
+      </div>
+    </>
+  );
+};
+
+export default DarkMode;
